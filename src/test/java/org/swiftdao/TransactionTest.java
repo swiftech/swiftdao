ab13085c125ac684d3aa8a533b55bdb29654d116
swiftech <awsksupport@gmail.com>
2015-10-06 01:39:16 +0800

    实现了分布式缓存会话。但是由于业务系统根据server_id区分玩家帐号，所以不能共享会话

--- src/test/java/org/swiftdao/TransactionTest.java
+++ src/test/java/org/swiftdao/TransactionTest.java
@@ -2,12 +2,8 @@ package org.swiftdao;
 
 import com.company.NoTransactionDao;
 import infrastructure.BaseDaoTest;
-import org.apache.commons.lang3.RandomUtils;
 import org.junit.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.swiftdao.demo.entity.MockSingleKeyEntity;
-
-import java.util.List;
 
 /**
  * Created by allen on 15/9/16.
@@ -18,11 +14,10 @@ public class TransactionTest extends BaseDaoTest {
 	private static Long longId2;
 
 	@Autowired
-	NoTransactionDao noTransactionDao;
+	NoTransactionDao noTransactionDao; // 这个 DAO 所属的 package 未配置事务处理
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
-
 
 	}
 
@@ -33,14 +28,7 @@ public class TransactionTest extends BaseDaoTest {
 
 	@Before
 	public void setUp() {
-		List<MockSingleKeyEntity> keyedEntities = createKeyedEntities(8);
 
-		for (MockSingleKeyEntity keyedEntity : keyedEntities) {
-			keyedEntity.setId(System.currentTimeMillis() + RandomUtils.nextInt(1, 1000));
-			mockOrmDao.create(keyedEntity);
-		}
-
-		System.out.printf("创建 %d 个实体%n", keyedEntities.size());
 	}
 
 	@After
@@ -48,6 +36,9 @@ public class TransactionTest extends BaseDaoTest {
 
 	}
 
+	/**
+	 * 不受 Spring 事务管理，这个测试方法应该会抛出异常并中止。
+	 */
 	@Test
 	public void testNoTransaction() {
 		long count;
@@ -63,12 +54,24 @@ public class TransactionTest extends BaseDaoTest {
 		}
 	}
 
+	/**
+	 * Spring 事务管理
+	 */
 	@Test
-	public void testHibernateSession() {
+	public void testTransaction() {
+// 创建测试数据
+//		List<MockSingleKeyEntity> keyedEntities = createKeyedEntities(8);
+//
+//		for (MockSingleKeyEntity keyedEntity : keyedEntities) {
+//			keyedEntity.setId(System.currentTimeMillis() + RandomUtils.nextInt(1, 1000));
+//			mockOrmDao.create(keyedEntity);
+//		}
+//
+//		System.out.printf("创建 %d 个实体%n", keyedEntities.size());
 
 		long count;
 
-		for (int i = 0; i < 15; i++) {
+		for (int i = 0; i < 1; i++) {
 
 			count = mockOrmDao.countAll();
 
